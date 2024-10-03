1f29ca24b88a385740447a989dad4c7057909ea6
Liam <lfdoherty@gmail.com>
2024-10-04 01:50:44 +0800

    commit

--- main.js
+++ main.js
@@ -68,6 +68,7 @@ function handleMessageFromServer(data){
 }
 
 
+
 function handleVirtualFileUpdate(metadata, dataBuf){
 	if(metadata.path === 'task-app.html'){
 		console.log('TODO - got task-app.html virtual file update')
